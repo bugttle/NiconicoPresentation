cdb3c18e82031d8a29b90a3b70e72fe3c01aa17b
UQ Times <uqtimes@gmail.com>
2015-03-09 14:09:30 +0900

    Initial commmit.

---
+++ client/NiconicoPresentation/NiconicoPresentation/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NiconicoPresentation
+//
+//  Created by UQ Times on 3/7/15.
+//  Copyright (c) 2015 UQ Times. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@end
+
