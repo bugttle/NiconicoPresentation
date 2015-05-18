774cd420a456a20cd0b58e47be96a19be019ebbb
UQ Times <uqtimes@gmail.com>
2015-05-18 20:14:07 +0900

    少し修正

--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // config
 var config = require('config');
 
@@ -10,9 +12,31 @@ logger.setLevel(config.log.level);
 // httpd
 var http = require('http');
 var fs = require('fs');
-var static = require('node-static');
-var fileServer = new static.Server('public');
+var node_static = require('node-static');
+var fileServer = new node_static.Server('public');
 var events = require('events');
+
+//var os = require('os');
+//var ifaces = os.networkInterfaces();
+//
+//Object.keys(ifaces).forEach(function(ifname) {
+//    var alias = 0;
+//    ifaces[ifname].forEach(function(iface) {
+//        if ('IPv4' !== iface.family || iface.internal !== false) {
+//            // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+//          return;
+//        }
+//
+//        if (alias >= 1) {
+//          // this single interface has multiple ipv4 addresses
+//          console.log(ifname + ':' + alias, iface.address);
+//        } else {
+//          // this interface has only one ipv4 adress
+//          console.log(ifname, iface.address);
+//        }
+//    });
+//});
+
 
 //
 // httpd server
