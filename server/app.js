e16e15b28d6135bf99f2421c5a8ad0d1893b0765
bugttle <bugttle@gmail.com>
2018-05-16 12:55:24 +0900

    Support:  - multi screen  - on full screen window

--- server/app.js
+++ server/app.js
@@ -16,26 +16,27 @@ var node_static = require('node-static');
 var fileServer = new node_static.Server('public');
 var events = require('events');
 
-//var os = require('os');
-//var ifaces = os.networkInterfaces();
-//
-//Object.keys(ifaces).forEach(function(ifname) {
-//    var alias = 0;
-//    ifaces[ifname].forEach(function(iface) {
-//        if ('IPv4' !== iface.family || iface.internal !== false) {
-//            // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-//          return;
-//        }
-//
-//        if (alias >= 1) {
-//          // this single interface has multiple ipv4 addresses
-//          console.log(ifname + ':' + alias, iface.address);
-//        } else {
-//          // this interface has only one ipv4 adress
-//          console.log(ifname, iface.address);
-//        }
-//    });
-//});
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function(ifname) {
+    var alias = 0;
+    ifaces[ifname].forEach(function(iface) {
+        if ('IPv4' !== iface.family || iface.internal !== false) {
+            // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+          return;
+        }
+
+        if (alias >= 1) {
+          // this single interface has multiple ipv4 addresses
+          console.log(ifname + ':' + alias, iface.address);
+        } else {
+          // this interface has only one ipv4 adress
+          console.log(ifname, iface.address);
+        }
+        ++alias;
+    });
+});
 
 
 //
