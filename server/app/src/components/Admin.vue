7237a886a31db2758f62f5b7d2b96c94a40811bc
bugttle <bugttle@gmail.com>
2019-02-19 01:21:00 +0900

    とりあえず

--- server/app/src/components/Admin.vue
+++ server/app/src/components/Admin.vue
@@ -1,114 +1,111 @@
 <template>
-    <div class="uk-container uk-container-center uk-text-center">
-        <div class="panel-heading">
-            <!-- Key event -->
-            <button type="button" id="left-arrow" class="uk-button uk-button-primary"
-                    v-on:click="sendKeyEvent" value="LeftArrow">
-                <font-awesome-icon icon="arrow-left"/>
-            </button>
-            <button type="button" id="right-arrow" class="uk-button uk-button-primary"
-                    v-on:click="sendKeyEvent" value="RightArrow">
-                <font-awesome-icon icon="arrow-right"/>
-            </button>
-
-            <!-- Like -->
-            <a href="#" v-on:click="sendLike">
-                <img src="../assets/like.png" alt="Like" class="like-image"/>
-            </a>
-            <span class="uk-badge">{{likeCount}}</span>
-
-            <!-- Test -->
-            <button type="button" id="test" class="uk-button uk-button-primary" v-on:click="sendTest">
-                <font-awesome-icon icon="bug"/>
-            </button>
-        </div>
-        <div class="panel-body">
+    <div>
+        <nav class="uk-navbar uk-margin">
+            <div class="uk-navbar-left">
+                <!-- Key event -->
+                <div class="uk-navbar-item">
+                    <button type="button" id="left-arrow" class="uk-button uk-button-primary"
+                            v-on:click="sendKeyEvent" value="LeftArrow">
+                        <font-awesome-icon icon="arrow-left"/>
+                    </button>
+                    <button type="button" id="right-arrow" class="uk-button uk-button-primary uk-margin-left"
+                            v-on:click="sendKeyEvent" value="RightArrow">
+                        <font-awesome-icon icon="arrow-right"/>
+                    </button>
+                </div>
+            </div>
+            <div class="uk-navbar-right">
+                <!-- Like -->
+                <div class="uk-navbar-item">
+                    <a href="#" v-on:click="sendLike">
+                        <img src="../assets/like.png" alt="Like" class="like-image"/>
+                    </a>
+                    <span class="uk-badge">{{likeCount}}</span>
+                </div>
+                <!-- Test -->
+                <div class="uk-navbar-item">
+                    <button type="button" id="test" class="uk-button uk-button-primary" v-on:click="sendTest">
+                        <font-awesome-icon icon="bug"/>
+                    </button>
+                </div>
+            </div>
+        </nav>
+        <div class="uk-container uk-container-center uk-text-center">
             <!-- Comment -->
             <form class="uk-form-horizontal" @submit.prevent="sendComment">
-                <div>
-                    <input type="text" v-model="comment" class="uk-input uk-form-width-medium"
+                <div class="uk-flex">
+                    <input type="text" v-model="comment" class="uk-input"
                            placeholder="Send a comment">
-                    <button class="uk-button uk-button-primary" type="submit">Send</button>
+                    <button class="uk-button uk-button-primary uk-width-1-4" type="submit">Send</button>
                 </div>
             </form>
-        </div>
 
-        <!-- Received comments -->
-        <ul>
-            <li class="list-group-item" v-for="(data, index) in receivedComments" :key="index">
-                ({{data.date}}) {{data.text}}
-            </li>
-        </ul>
+            <!-- Received comments -->
+            <ul class="uk-list uk-list-divider">
+                <li v-for="(data, index) in receivedComments" :key="index">
+                    <div class="uk-flex">
+                        <span class="uk-width-1-4 uk-text-right">{{data.date}}</span><span
+                            class="uk-width-auto uk-text-left uk-margin-left">{{data.text}}</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
-    import io from 'socket.io-client'
-    import queryString from 'query-string'
+  import io from 'socket.io-client'
+  import queryString from 'query-string'
 
-    export default {
-        name: 'admin',
-        data() {
-            return {
-                likeCount: 0,
-                comment: '',
-                receivedComments: [],
-            }
-        },
-        mounted() {
-            this.socket = io(location.origin)
+  export default {
+    name: 'admin',
+    data() {
+      return {
+        likeCount: 0,
+        comment: '',
+        receivedComments: [{date: 'YYYY-MM-DD 111111', text: 'aaaaaaa'}, {date: 'YYYY-MM-DD 222222', text: 'aaaaaaa'}],
+      }
+    },
+    mounted() {
+      this.socket = io(location.origin)
 
-            this.socket.on('init', (data) => {
-                this.likeCount = data.like
-            })
-            this.socket.on('like', (data) => {
-                this.likeCount = data.count
-            })
-            this.socket.on('comment', (data) => {
-                this.receivedComments.push(data)
-            })
+      this.socket.on('init', (data) => {
+        this.likeCount = data.like
+      })
+      this.socket.on('like', (data) => {
+        this.likeCount = data.count
+      })
+      this.socket.on('comment', (data) => {
+        this.receivedComments.push(data)
+      })
 
-            // Admin auth key
-            const query = queryString.parse(location.search)
-            this.socket.emit('join', {key: query.key})
-        },
-        methods: {
-            sendTest() {
-                this.socket.emit('test')
-            },
-            sendLike() {
-                this.socket.emit('like')
-            },
-            sendComment() {
-                if (this.comment) {
-                    this.socket.emit('comment', {'text': this.comment})
-                    this.comment = ''
-                }
-            },
-            sendKeyEvent(element) {
-                this.socket.emit('key', {'code': element.value})
-            },
+      // Admin auth key
+      const query = queryString.parse(location.search)
+      this.socket.emit('join', {key: query.key})
+    },
+    methods: {
+      sendTest() {
+        this.socket.emit('test')
+      },
+      sendLike() {
+        this.socket.emit('like')
+      },
+      sendComment() {
+        if (this.comment) {
+          this.socket.emit('comment', {'text': this.comment})
+          this.comment = ''
         }
+      },
+      sendKeyEvent(element) {
+        this.socket.emit('key', {'code': element.value})
+      },
     }
+  }
 </script>
 
 <style scoped>
     .like-image {
         width: 44px;
         height: 44px;
-    }
-
-    .like-image {
-        margin: 0 0 0 5em;
-    }
-
-    #left-arrow {
-        margin: 0 1em 0 0;
-    }
-
-    #test {
-        position: absolute;
-        right: 0;
-        top: 1.2em;
     }
 </style>
--- server/app/src/components/User.vue
+++ server/app/src/components/User.vue
@@ -1,65 +1,67 @@
 <template>
     <div class="uk-container uk-container-center uk-text-center">
-        <h1 class="uk-heading-large">PRESENTATION</h1>
+        <h1 class="uk-heading-divider uk-text-uppercase uk-text-bold">PRESENTATION</h1>
 
         <!-- Like -->
-        <div class="uk-inline">
-            <!--<div class="img-overlay like-image">-->
-            <a href="#" v-on:click="sendLike">
-                <img src="../assets/like.png" alt="Like"/>
-                <!--<div class="project-overlay">-->
-                <div class="uk-overlay uk-position-bottom-center uk-padding-remove">
-                    <span class="uk-badge">{{likeCount}}</span>
-                </div>
-            </a>
+        <div class="uk-card uk-card-default uk-margin">
+            <div class="uk-card-media-top">
+                <a href="#" v-on:click="sendLike">
+                    <img src="../assets/like.png" alt="Like" class="like-image"/>
+                </a>
+            </div>
+            <div class="uk-card-media-bottom uk-text-large uk-text-bold">{{likeCount}}</div>
         </div>
 
         <!-- Comment -->
         <form class="uk-form-horizontal" @submit.prevent="sendComment">
-            <div>
-                <input type="text" v-model="comment" class="uk-input uk-form-width-medium"
+            <div class="uk-flex">
+                <input type="text" v-model="comment" class="uk-input"
                        placeholder="Please give me a feedback!">
-                <button class="uk-button uk-button-primary" type="submit">Send</button>
+                <button class="uk-button uk-button-primary uk-width-1-4" type="submit">Send</button>
             </div>
         </form>
     </div>
 </template>
 
 <script>
-    import io from 'socket.io-client'
-    import 'uikit/dist/css/uikit.css'
+  import io from 'socket.io-client'
+  import 'uikit/dist/css/uikit.css'
 
-    export default {
-        name: 'User',
-        data() {
-            return {
-                likeCount: 0,
-                comment: '',
-            }
-        },
-        mounted() {
-            this.socket = io(location.origin)
-            this.socket.on('init', function (data) {
-                this.likeCount = data.like
-            })
-            this.socket.on('like', function (data) {
-                this.likeCount = data.count
-            })
-            this.socket.emit('join')
-        },
-        methods: {
-            sendLike() {
-                this.socket.emit('like')
-            },
-            sendComment() {
-                if (this.comment) {
-                    this.socket.emit('comment', {text: this.comment})
-                    this.comment = ''
-                }
-            },
+  export default {
+    name: 'User',
+    data() {
+      return {
+        likeCount: 0,
+        comment: '',
+      }
+    },
+    mounted() {
+      this.socket = io(location.origin)
+      this.socket.on('init', function(data) {
+        this.likeCount = data.like
+      })
+      this.socket.on('like', function(data) {
+        this.likeCount = data.count
+      })
+      this.socket.emit('join')
+    },
+    methods: {
+      sendLike() {
+        this.socket.emit('like')
+      },
+      sendComment() {
+        if (this.comment) {
+          this.socket.emit('comment', {text: this.comment})
+          this.comment = ''
         }
+      },
     }
+  }
 </script>
 
 <style scoped>
+    .like-image {
+        width: 260px;
+        height: 260px;
+    }
 </style>
